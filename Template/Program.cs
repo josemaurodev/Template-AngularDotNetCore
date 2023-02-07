53bf9a6a88a3802e51d2233bde50c56bb663053f
josemaurodev <josemauro.dev@gmail.com>
2023-02-07 01:04:09 -0300

    Commit

--- Template/Program.cs
+++ Template/Program.cs
@@ -1,8 +1,17 @@
+using FluentAssertions.Common;
+using Microsoft.EntityFrameworkCore;
+using Template.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<TemplateContext>();
+
+var connectionString = builder.Configuration.GetConnectionString("TemplateDB");
+builder.Services.AddDbContext<TemplateContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString(connectionString)).EnableSensitiveDataLogging());
+
 
 var app = builder.Build();
 
